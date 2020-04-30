6d17e93928024361780a1b38554ccb479d6f6e99
pvijayakumar6 <64572369+pvijayakumar6@users.noreply.github.com>
2020-04-30 10:11:40 +0530

    Add files via upload

---
+++ Build-SCM-Containers/src/main/java/com/isquarebsys/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.isquarebsys;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ 
+@RestController
+public class EmployeeController
+{
+    @RequestMapping("/apis/employees")
+    public List<Employee> getEmployees()
+    {
+        List<Employee> employeesList = new ArrayList<Employee>();
+        employeesList.add(new Employee(1,"vijay","p","isquarebsys@gmail.com"));
+        employeesList.add(new Employee(2,"akshay","a","akshay@gmail.com"));
+        return employeesList;
+    }
+}
+
